044d6cb23649924996e3db28289a802242f75a36
snadboy <dschless@gmail.com>
2024-02-18 21:55:38 -0600

    Added try...except's to router functions

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 import logging.config
 
 from log_secret import SecretFilter
+from plex_funcs import PlexFuncs
 
 logging.config.fileConfig("log_config.ini", disable_existing_loggers=False)
 logging.getLogger().addFilter(SecretFilter())
@@ -13,7 +14,7 @@ from contextlib import asynccontextmanager
 from time import perf_counter
 from typing import Any, Callable, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 
 from config import Config
@@ -56,34 +57,45 @@ async def autosync():
 
 app = FastAPI(lifespan=app_lifespan)
 
+@app.get("/")
+async def root():
+    return {"message": "Heartbeat"}
 
 @app.post("/sonarr_event/")
 async def sonarr_event(data: dict):
-    if data.get("eventType") in [
-        member.value for member in SonarrEventType.__members__.values()
-    ]:
+    if data.get("eventType", "") not in [member.value for member in SonarrEventType.__members__.values()]:
+        raise HTTPException(status_code=400, 
+            detail=f"Invalid eventType: {data.get('eventType')} - must be one of: {', '.join([member.value for member in SonarrEventType.__members__.values()])}")
+
+    try:
         title = data.get("series", {}).get("title")
         results = await asyncio.to_thread(lambda: sonarr_to_plex(title=title))
         logger.info(f"sonarr_event results: {results}")
-        # asyncio.create_task(in_thread(lambda: Shows(Config.PLEX_URL, Config.PLEX_TOKEN, Config.SONARR_URL, Config.SONARR_API_KEY).sonarr_to_plex_batch(title=title), f'sonarr_event: {data.get('eventType')}'))
-    return JSONResponse(content={"message": "processed successfully"})
+        return JSONResponse(content={"message": "processed successfully"})
+    except Exception as e:
+        HTTPException(status_code=500, detail=f"Exception: {e}")
 
 
 @app.post("/fix_labels/")
 async def fix_labels(title: Optional[str] = ""):
-    logger.info(f"fix_labels: {title}")
-    return await asyncio.to_thread(lambda: sonarr_to_plex(title=title), "fix_labels")
-    # return await in_thread(lambda: Shows(Config.PLEX_URL, Config.PLEX_TOKEN, Config.SONARR_URL, Config.SONARR_API_KEY).sonarr_to_plex_batch(title=title), 'fix_labels')
+    try:
+        logger.info(f"fix_labels: {title}")
+        return await asyncio.to_thread(lambda: sonarr_to_plex(title=title), "fix_labels")
+    except Exception as e:
+        HTTPException(status_code=500, detail=f"Exception: {e}")
 
 
 @app.get("/plex_series/")
 async def plex_series(title: Optional[str] = None, days: Optional[int] = None):
-    logger.info(f"plex_series: title: {title} - days: {days}")
-    return await asyncio.to_thread(
-        lambda: SonarrFuncs().plex_series_search(title=title, days=days), "plex_series"
-    )
-    # return await in_thread(lambda: Shows(Config.PLEX_URL, Config.PLEX_TOKEN, Config.SONARR_URL, Config.SONARR_API_KEY).plex_series_search(title=title, days=days), 'plex_series')
+    if days and days < 0:
+        raise HTTPException(status_code=400, 
+            detail=f"Invalid value for days: {days} - if present, it must be zero or a positive integer")
 
+    try:
+        logger.info(f"plex_series: title: {title} - days: {days}")
+        return await asyncio.to_thread(lambda: PlexFuncs().plex_series_search(title=title, days=days))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Exception: {e}")
 
 if __name__ == "__main__":
     import logging.config
@@ -93,5 +105,5 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "app:app", host="0.0.0.0", port=8000, log_level=logging.INFO, reload=True
+        "app:app", host="0.0.0.0", port=8001, log_level=logging.INFO, reload=True
     ),
